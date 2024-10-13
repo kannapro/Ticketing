5bba5b5d68721ff76c4af9ae5b17d70e362d0bd1
Kannan <147408745+kannapro@users.noreply.github.com>
2024-10-13 23:10:53 +0530

    Project Setup Done for Each Layer (#1)

---
+++ src/TicketManagement/TM.Presentation/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TM.Application;
+using TM.Infrastructure;
+using TM.Persistence;
+
+namespace TM.Presentation;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services
+            .AddApplication()
+            .AddInfrastructure()
+            .AddPersistence();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    public static void UsePresentation(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapGet("/", () => "Welcome to CodeDaze API! Happy Coding");
+    }
+}
