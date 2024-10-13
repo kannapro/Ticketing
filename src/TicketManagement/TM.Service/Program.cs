c53b755506f02de0b75a0d2c584f11406dfbe825
Code Daze <kannan.netcore@outlook.com>
2024-10-14 00:41:03 +0530

    Added Code for Serilog Seq and OpenTelemetry

--- src/TicketManagement/TM.Service/Program.cs
+++ src/TicketManagement/TM.Service/Program.cs
@@ -1,3 +1,6 @@
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 using TM.Presentation;
 
@@ -7,7 +10,21 @@ builder.Host.UseSerilog((context, config) =>
     config.ReadFrom.Configuration(context.Configuration);
 });
 builder.Services.AddPresentation();
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource=> resource.AddService("TM.Service"))
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddOtlpExporter(opt =>
+            {
+                opt.Endpoint = new Uri("http://localhost:9090/ingest/otlp/v1/traces");
+                opt.Protocol = OtlpExportProtocol.HttpProtobuf;
+            });
 
+        
+    });
 var app = builder.Build();
 app.UsePresentation();
 app.UseSerilogRequestLogging();
