28e7ca5868cc933e522c96dabd8611b87f09c224
Kannan <147408745+kannapro@users.noreply.github.com>
2024-10-13 23:18:40 +0530

    Added Code for Serilog Console and File (#2)

--- src/TicketManagement/TM.Service/Program.cs
+++ src/TicketManagement/TM.Service/Program.cs
@@ -1,10 +1,14 @@
+using Serilog;
 using TM.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Host.UseSerilog((context, config) =>
+{
+    config.ReadFrom.Configuration(context.Configuration);
+});
 builder.Services.AddPresentation();
 
 var app = builder.Build();
 app.UsePresentation();
-
+app.UseSerilogRequestLogging();
 app.Run();
