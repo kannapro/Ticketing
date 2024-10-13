0cd1143e5f40ae79d272222ef3abbdb84c8a0844
Kannan <147408745+kannapro@users.noreply.github.com>
2024-10-14 02:18:19 +0530

    Added code Handling Endpoint in Seprate files in Minimal API (#4)

---
+++ src/TicketManagement/TM.Presentation/Endpoints/HomeEndpoint .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using TM.Presentation.Handlers;
+
+namespace TM.Presentation.Endpoints;
+
+public class HomeEndpoint : IEndpoint
+{
+    public void MapEndpoints(WebApplication app)
+    {
+        app.MapGet("/", HomeEndpointHandlers.HandleHome);
+    }
+}
---
+++ src/TicketManagement/TM.Presentation/Handlers/HomeEndpointHandlers.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TM.Presentation.Handlers;
+
+public static class HomeEndpointHandlers
+{
+    public static IResult HandleHome()
+    {
+        return Results.Ok("Welcome to Code Daze! 😀");
+    }
+}
